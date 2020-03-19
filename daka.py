9bfddeec6ba4f4b822daeabbfcc353d5601e8243
Tishacy <Tishacy@gmail.com>
2020-03-19 10:41:58 +0800

    update form field change: sfqrxxss.

--- daka.py
+++ daka.py
@@ -60,6 +60,7 @@ class DaKa(object):
         new_info['jrdqjcqk[]'] = 0
         new_info['sfsqhzjkk'] = 1   # 是否申领杭州健康码
         new_info['sqhzjkkys'] = 1   # 杭州健康吗颜色，1:绿色 2:红色 3:黄色
+        new_info['sfqrxxss'] = 1    # 是否确认信息属实
         self.info = new_info
         return new_info
 
